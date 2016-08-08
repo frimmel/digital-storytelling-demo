868064d3edc206ac6e9c2d6ec1da12f2a384609f
John Frimmel <jrfrimme@unca.edu>
2016-08-08 13:52:00 -0400

    adds basic slider code

---
+++ slider/slider.js
@@ -0,0 +1,18 @@
+var imageUrls = ["../images/landuse_76.jpg", "../images/landuse_06.jpg", "../images/landuse_30.jpg"];
+
+function preloadImages () {
+  var i, image;
+  for (i = 0; i < imageUrls.length; i++) {
+    image = document.createElement("img");
+    image.setAttribute("src", imageUrls[i]);
+  }
+}
+
+function changeImage (e) {
+  var url = imageUrls[e.target.value];
+  var image = document.getElementById("slider-image");
+  image.setAttribute("src", url);
+}
+
+window.onload = preloadImages;
+document.getElementById("slider-input").addEventListener("input", changeImage);
